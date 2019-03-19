b6f0fa787b3c4ff864c3880491d5d3aa063d39c1
RaulSevi <rsevig00@estudiantes.unileon.es>
2019-03-19 14:03:33 +0100

    Added some methods

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -1,26 +1,94 @@
 package es.unileon.prg.date;
 
+/**
+ *
+ *Clase Date
+ *
+ *@version
+ *@author prog1
+ */
+
+
 public class Date {
 
-	private int day;
-	private int month;
-	private int year;
+	private int _day;
+	private int _month;
+	private int _year;
+	
 
 	public Date(int day, int month, int year) throws DateException{
-		this.year = year;
+		this._year = year;
+		if ((month) < 1 || (month > 12)) {
+			throw new DateException("Mes " + month + " no valido" +
+					" Valores posibles entre 1 y 12.");
+		} else {
+			this._month = month;
+		}
+		//TODO falta comprobar el dia
+		if ((day) < 1 || (day > getDaysOfMonth)) {
+			throw new DateException("Dia " + day + " no valido" +
+					" Valores posibles entre 1 y 12.");
+		} else {
+			this._day = day;
+		}
+		this._day = day;
+	}
+	public void setDay(int day){
+		if ((day) < 1 || (day > getDaysOfMonth)) {
+			throw new DateException("Dia " + day + " no valido" +
+					" Valores posibles entre 1 y 12.");
+		} else {
+			this._day = day;
+		}
+
+	}
+
+	/**
+	 * Clase setMonth, setea el atributo 
+	 *
+	 *
+	 *
+         */
+
+
+	public void setMonth(int month) throws DateException{
 		if (month < 1 || month > 12) {
 			throw new DateException("Mes " + month + " no valido" +
 					" Valores posibles entre 1 y 12.");
 		} else {
-			this.month = month;
+			this._month = month;
 		}
-		//TODO falta comprobar el dia
-		this.day = day;
 	}
 
+	public int getDaysOfMonth(int month){
+		int number =0;
+			switch(month){
+				case 1: 
+				case 3:
+				case 5:
+				case 7: 
+				case 8:
+				case 10:
+				case 12:
+			number=31;
+				break;
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+			number=30;
+				break;
+				case 2:
+			number=28;
+				break;
+				default: 
+			number=-1;
+		}
+	}
+	
 	@Override
 	public String toString() {
-		return this.day + "/" + this.month + "/" + this.year;
+		return this._day + "/" + this._month + "/" + this._year;
 	}
 
 }
