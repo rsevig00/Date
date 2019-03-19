5c226ef820d608b0bbca28d2451aafaea18d13d3
RaulSevi <rsevig00@estudiantes.unileon.es>
2019-03-19 14:30:17 +0100

    Added more methods

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -25,7 +25,7 @@ public class Date {
 			this._month = month;
 		}
 		//TODO falta comprobar el dia
-		if ((day) < 1 || (day > getDaysOfMonth)) {
+		if ((day) < 1 || (day > getDaysOfMonth(month))) throws DateException{
 			throw new DateException("Dia " + day + " no valido" +
 					" Valores posibles entre 1 y 12.");
 		} else {
@@ -33,15 +33,25 @@ public class Date {
 		}
 		this._day = day;
 	}
-	public void setDay(int day){
-		if ((day) < 1 || (day > getDaysOfMonth)) {
+
+	public void setDay(int day)throws DateException{
+		if ((day) < 1 || (day > getDaysOfMonth(day))) {
 			throw new DateException("Dia " + day + " no valido" +
 					" Valores posibles entre 1 y 12.");
 		} else {
 			this._day = day;
 		}
-
 	}
+
+	public Boolean isSameYear(int year){
+		Boolean esElMismoDia = false;
+			if (this._year == year){
+				esElMismoDia = true;
+			}
+		return esElMismoDia;
+	}
+
+	public Boolean isSameMonth{ 
 
 	/**
 	 * Clase setMonth, setea el atributo 
@@ -60,7 +70,7 @@ public class Date {
 		}
 	}
 
-	public int getDaysOfMonth(int month){
+	public int getDaysOfMonth(int month) throws DateException{
 		int number =0;
 			switch(month){
 				case 1: 
@@ -80,6 +90,35 @@ public class Date {
 				break;
 				case 2:
 			number=28;
+				break;
+				default: 
+			number=-1;
+		}
+		return number;
+	}
+	private String printMonthName(int month){
+		String nombreMes="";
+		switch(month){
+				case 1: 
+			nombreMes="Enero";
+				break;
+				case 3:
+				case 5:
+				case 7: 
+				case 8:
+				case 10:
+				case 12:
+			number=31;
+				break;
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+			number=30;
+				break;
+				case 2:
+			number=28;
+			nombreMes="Febrero";
 				break;
 				default: 
 			number=-1;
--- src/es/unileon/prg/date/MainDate.java
+++ src/es/unileon/prg/date/MainDate.java
@@ -5,7 +5,8 @@ public class MainDate {
 	public static void main(String[] args) {
 
 		Date today;
-
+			int meses = (int) (Math.random()*12)+1;
+			System.out.printnln(meses);
 		try {
 			today = new Date(99, 14, 2017);
 			System.out.println(today.toString());
