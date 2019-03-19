793eae33f22389fa9b3dc851ad50d73a6654a021
RaulSevi <rsevig00@estudiantes.unileon.es>
2019-03-19 13:28:09 +0100

    Initial Commit

---
+++ src/es/unileon/prg/date/Date.java
@@ -0,0 +1,26 @@
+package es.unileon.prg.date;
+
+public class Date {
+
+	private int day;
+	private int month;
+	private int year;
+
+	public Date(int day, int month, int year) throws DateException{
+		this.year = year;
+		if (month < 1 || month > 12) {
+			throw new DateException("Mes " + month + " no valido" +
+					" Valores posibles entre 1 y 12.");
+		} else {
+			this.month = month;
+		}
+		//TODO falta comprobar el dia
+		this.day = day;
+	}
+
+	@Override
+	public String toString() {
+		return this.day + "/" + this.month + "/" + this.year;
+	}
+
+}
